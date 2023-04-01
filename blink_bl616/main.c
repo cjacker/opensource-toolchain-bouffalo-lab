5218057d2ee07fb07ac62684201c70c61dd8988b
Cjacker <cjacker@foxmail.com>
2023-04-01 18:58:41 +0800

    add bouffalo_sdk patch for M0P bl618 and blink demo

--- blink_bl616/main.c
+++ blink_bl616/main.c
@@ -1,4 +1,5 @@
 // Blink 2 LEDs of M0S Dock connect to GPIO 27 and 28
+// Blink 1 LED of M0P Dock connect to GPIO 21
 #include "bflb_gpio.h"
 #include "board.h"
 
@@ -12,13 +13,24 @@ int main(void)
     printf("gpio output\r\n");
     bflb_gpio_init(gpio, GPIO_PIN_27, GPIO_OUTPUT | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_0);
     bflb_gpio_init(gpio, GPIO_PIN_28, GPIO_OUTPUT | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_0);
+    // for Sipeed M0P (bl618)
+    bflb_gpio_init(gpio, GPIO_PIN_21, GPIO_OUTPUT | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_0);
 
     while (1) {
         bflb_gpio_set(gpio, GPIO_PIN_27);
         bflb_gpio_reset(gpio, GPIO_PIN_28);
+
+        // for Sipeed M0P (bl618)
+        bflb_gpio_set(gpio, GPIO_PIN_21);
+
         bflb_mtimer_delay_ms(100);
+
         bflb_gpio_reset(gpio, GPIO_PIN_27);
         bflb_gpio_set(gpio, GPIO_PIN_28);
+
+        // for Sipeed M0P (bl618)
+        bflb_gpio_reset(gpio, GPIO_PIN_21);
+
         bflb_mtimer_delay_ms(100);
     }
 }
